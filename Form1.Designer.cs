ece68a6849047b8cefd792c2cfd7c8733eeef5da
robinlyang <robiny@gmail.com>
2016-11-21 18:37:48 -0700

    Initial and Complete Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace NETTwo_SortedListDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.phone = new System.Windows.Forms.TextBox();
+            this.read = new System.Windows.Forms.Button();
+            this.message = new System.Windows.Forms.Label();
+            this.add = new System.Windows.Forms.Button();
+            this.change = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID:";
+            // 
+            // id
+            // 
+            this.id.Location = new System.Drawing.Point(97, 22);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(100, 22);
+            this.id.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name:";
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(97, 61);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(462, 22);
+            this.name.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Phone:";
+            // 
+            // phone
+            // 
+            this.phone.Location = new System.Drawing.Point(97, 100);
+            this.phone.Name = "phone";
+            this.phone.Size = new System.Drawing.Size(203, 22);
+            this.phone.TabIndex = 5;
+            // 
+            // read
+            // 
+            this.read.Location = new System.Drawing.Point(241, 22);
+            this.read.Name = "read";
+            this.read.Size = new System.Drawing.Size(75, 23);
+            this.read.TabIndex = 6;
+            this.read.Text = "Read";
+            this.read.UseVisualStyleBackColor = true;
+            this.read.Click += new System.EventHandler(this.read_Click);
+            // 
+            // message
+            // 
+            this.message.AutoSize = true;
+            this.message.Location = new System.Drawing.Point(413, 103);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(65, 17);
+            this.message.TabIndex = 7;
+            this.message.Text = "Message";
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(322, 22);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 23);
+            this.add.TabIndex = 8;
+            this.add.Text = "Add";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // change
+            // 
+            this.change.Location = new System.Drawing.Point(403, 22);
+            this.change.Name = "change";
+            this.change.Size = new System.Drawing.Size(75, 23);
+            this.change.TabIndex = 9;
+            this.change.Text = "Change";
+            this.change.UseVisualStyleBackColor = true;
+            this.change.Click += new System.EventHandler(this.change_Click);
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(484, 22);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(75, 23);
+            this.delete.TabIndex = 10;
+            this.delete.Text = "Delete";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 153);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.change);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.read);
+            this.Controls.Add(this.phone);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.id);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox phone;
+        private System.Windows.Forms.Button read;
+        private System.Windows.Forms.Label message;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button change;
+        private System.Windows.Forms.Button delete;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace NETTwo_SortedListDB
+{
+    public partial class Form1 : Form
+    {
+        SortedList<string, person> database = new SortedList<string, person>(); 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {//add button
+            person record = new person();
+            record.name = name.Text;
+            record.phone = Convert.ToInt64(phone.Text);
+            database[id.Text] = record;
+            message.Text = "Added";
+        }
+
+        private void change_Click(object sender, EventArgs e)
+        {//change button
+            person record = new person();
+            record.name = name.Text;
+            record.phone = Convert.ToInt64(phone.Text);
+            database[id.Text] = record;
+            message.Text = "Changed";
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {//delete button
+            database.Remove(id.Text);
+            message.Text = "Deleted";
+            id.Text = "";
+            name.Text = "";
+            phone.Text = "";
+        }
+
+        private void read_Click(object sender, EventArgs e)
+        {//read button
+            if(database.ContainsKey(id.Text))
+            {
+                name.Text = database[id.Text].name;
+                phone.Text = database[id.Text].phone.ToString();
+                message.Text = "";
+            }
+            else
+            {
+                message.Text = "Cannot find";
+                id.Text = "";
+                name.Text = "";
+                phone.Text = "";
+            }
+        }
+    }
+    class person
+    {
+        public string name;
+        public long phone;
+    }
+}
